bf8948ad6463872005451d594ee83a0ddd7aa124
Anthony Kinsey <anthony@slickage.com>
2014-06-23 16:50:14 -1000

    Updates

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(3333);
