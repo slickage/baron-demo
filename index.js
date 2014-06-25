835771d788ed4935d846e62891066cd3ca20d374
Anthony Kinsey <anthony@slickage.com>
2014-06-24 18:59:20 -1000

    Added support for posting to baron for invoice creation

--- index.js
+++ index.js
@@ -2,24 +2,44 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var rateLimiter = require('rate-limiter');
+var request = require('request');
 var app = express();
 
+// Config settings
+var baronInvoiceRoute = 'http://localhost:3000/invoices';
+var baronApiKey = 'youshouldreallychangethis';
+var port = 3333;
+
+// Rate Limiting config (1 request every 60 seconds)
 var rules = [
-  ['/invoice', 'post', 1, 60, true]
+  ['/invoice', 'post', 5, 60, true]
 ];
 
+// Middleware
 app.use(rateLimiter.expressMiddleware(rules));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Submitting the invoice form posts to here so we can add in the api_key
+// and rate limit user's ability to create invoices within baron.
 app.post('/invoice', function(req, res) {
   var invoice = req.body;
-  console.log(invoice);
-  res.write(invoice.currency);
-  res.end();
+  invoice.api_key = baronApiKey;
+  request.post({ url: baronInvoiceRoute, form:invoice }, function(err, baronRes, body) {
+    if (!err && baronRes && baronRes.statusCode && baronRes.statusCode === 200) {
+      res.status(200).write(body);
+      res.end();
+    }
+    else if(err && !baronRes) {
+      res.status(500).write(err.message);
+      res.end();
+    }
+    else {
+      res.status(baronRes.statusCode).write(err.message);
+      res.end();
+    }
+  });
 });
 
-
-
-app.listen(3333);
+app.listen(port);
